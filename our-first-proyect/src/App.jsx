b5256ff260d77a8197b8a551b7af238e2bc119ad
Noemí Mira <noemi.miranavarro@gmail.com>
2025-06-06 10:52:54 +0200

    our first proyect1

---
+++ our-first-proyect/src/App.jsx
@@ -0,0 +1,31 @@
+import './App.css';
+import discordLogo from './assets/logo.png'
+import burguerMenu from './assets/burguer.png'
+import peopleImg from './assets/peopleImg.png'
+
+function App() {
+  return (
+    <div className="App">
+      <nav>
+        <img src={discordLogo} alt="logo"/>
+        <img src={burguerMenu} alt="burguer"/>
+      </nav>
+      <h1>IMAGE A PLACE...</h1>
+      <p>
+        ...where you can belong to a school club, a gaming group,or a
+        worldwide art community. Where just you and a handful of
+        friends can spend time together. A place that makes it easy to 
+        talk every day and hang out more often.
+      </p>
+      <section id="btn-container">
+        <button>Download for Mac</button>
+        <button id="discond-btn">Open Discord in your Browser</button>
+      </section>
+      <section>
+        <img src={peopleImg} alt="people image" className='people-img'/>
+      </section>
+    </div>
+  );
+}
+
+export default App;
