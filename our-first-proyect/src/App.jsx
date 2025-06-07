f33fab0af4a684e606ef8b6b7a88530eebd99dc0
Noemí Mira <noemi.miranavarro@gmail.com>
2025-06-07 13:56:14 +0200

    done

--- our-first-proyect/src/App.jsx
+++ our-first-proyect/src/App.jsx
@@ -1,16 +1,19 @@
 import './App.css';
-import discordLogo from './assets/logo.png'
-import burguerMenu from './assets/burguer.png'
-import peopleImg from './assets/peopleImg.png'
+import discordLogo from './assets/logo.png';
+import burguerMenu from './assets/burguer.png';
+import peopleImg from './assets/peopleImg.png';
 
 function App() {
   return (
-    <div className="App">
+    <>
       <nav>
         <img src={discordLogo} alt="logo"/>
         <img src={burguerMenu} alt="burguer"/>
       </nav>
-      <h1>IMAGE A PLACE...</h1>
+      <h1>IMAGE A 
+        <br />
+        <br />
+        PLACE...</h1>
       <p>
         ...where you can belong to a school club, a gaming group,or a
         worldwide art community. Where just you and a handful of
@@ -19,12 +22,12 @@ function App() {
       </p>
       <section id="btn-container">
         <button>Download for Mac</button>
-        <button id="discond-btn">Open Discord in your Browser</button>
+        <button id="discord-btn">Open Discord in your Browser</button>
       </section>
       <section>
         <img src={peopleImg} alt="people image" className='people-img'/>
       </section>
-    </div>
+    </>
   );
 }
 
